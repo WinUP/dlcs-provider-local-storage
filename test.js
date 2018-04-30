d7c59d53a307ca5a1ad484abfb097a131e593671
WinUP <l.gholoi@outlook.com>
2018-04-30 12:55:39 -0400

    BUG fix.

--- test.js
+++ test.js
@@ -3,6 +3,3 @@ var { ResourceManager } = require('@dlcs/core');
 
 const manager = new ResourceManager();
 manager.registerProtocol(new StorageProtocol());
-
-console.log(manager.request.to('cache:///test1/test2').submit('123').requireSync().metadata);
-console.log(manager.request.to('cache:///test1/test2').requireSync().metadata);
